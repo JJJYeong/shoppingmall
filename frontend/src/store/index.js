a0ad5de044c584d12ab300ca6e8f0e2b3d41f4cf
JJJYeong <bss05007@naver.com>
2024-02-01 15:18:36 +0900

    프론트엔드 세팅

---
+++ frontend/src/store/index.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import storage from 'redux-persist/lib/storage'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist'
+
+const rootReducer = combineReducers({
+    user: userReducer
+});
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: {
+        user: persistedReducer,
+        mddleware: getDefaultMiddleware => {
+            return ({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]  //serializableCheck: false
+                }
+            });
+        }
+    }
+})
+
+export const persistor = persistStore(store)
