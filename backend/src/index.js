1d97663e75395b9d410b449616e69485d9300826
JJJYeong <bss05007@naver.com>
2024-02-02 15:25:25 +0900

    백엔드 세팅

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 4000
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected...');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+app.get('/', (req, res, next) => {
+    res.send('Hello, World!');
+    // setImmediate(() => {
+    //     next(new Error('it is an error'));
+    // });
+});
+
+app.post('/', (req, res) => {
+    res.json(req.body);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(err.message || 'server error!');
+});
+
+app.use(express.static(path.join(__dirname, '../uploads')));
+
+app.listen(port, () => {
+    console.log(`Server Listening on ${port}`);
+});
